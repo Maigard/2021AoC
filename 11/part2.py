76924d31bef57d87465e6919968c310e43f6e55d
Ken Schmidt <kenneth.schmidt@pnnl.gov>
2021-12-11 15:47:27 -0800

    Day 11

---
+++ 11/part2.py
@@ -0,0 +1,46 @@
+from pprint import pprint
+def parse_input(filename: "str") -> "list":
+    with open(filename, "r") as file:
+        return [[int(char) for char in line.rstrip()] for line in file]
+
+dirs = (
+    (-1, -1), (-1, 0), (-1, +1),
+    ( 0, -1),          ( 0, +1),
+    (+1, -1), (+1, 0), (+1, +1)
+)
+
+def flash(octs: "list", x: "int", y: "int"):
+    for dir in dirs:
+        newx = x + dir[1]
+        newy = y + dir[0]
+        if 0 <= newy < len(octs) and 0 <= newx < len(octs[y]):
+            octs[newy][newx] += 1
+            if octs[newy][newx] == 10:
+                flash(octs, newx, newy)
+
+def check_all_flash(octs: "list") -> "bool":
+    for y in range(len(octs)):
+        for x in range(len(octs[y])):
+            if octs[y][x] != 0:
+                return False
+    return True
+
+def main():
+    octs = parse_input("11/inputs.txt")
+    flashes = 0
+    for step in range(1000):
+        for y in range(len(octs)):
+            for x in range(len(octs[y])):
+                octs[y][x] += 1
+                if octs[y][x] == 10:
+                    flash(octs, x, y)
+        for y in range(len(octs)):
+            for x in range(len(octs[y])):
+                if octs[y][x] > 9:
+                    flashes += 1
+                    octs[y][x] = 0
+        if check_all_flash(octs):
+            print(step+1)
+    print(flashes)
+
+main()
